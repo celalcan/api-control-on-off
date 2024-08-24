04266e1942c6c211d82d9998f57568d96ef8124f
celalcan <celalcanh.h.oglu@gmail.com>
2024-08-24 15:08:17 +0300

    Commit mesajınız

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const handleStatusChange = (newValue) => {
-    axios.patch('https://api-control-on-off.vercel.app/api/status', { value: newValue })
-      .then(response => {
-        console.log('Status updated:', response.data);
-      })
-      .catch(error => {
-        console.error('There was an error updating the status!', error);
+  const updateStatus = async (led, newValue) => {
+    try {
+      const response = await axios.patch('https://api-control-on-off.vercel.app/api/status', {
+        status: {
+          [led]: newValue
+        }
       });
+      console.log(`Status of ${led} updated:`, response.data);
+    } catch (error) {
+      console.error(`There was an error updating the ${led} status!`, error);
+    }
   };
 
   return (
     <div className="App">
-      <button onClick={() => handleStatusChange(1)}>On</button>
-      <button onClick={() => handleStatusChange(0)}>Off</button>
+      <div>
+        <button onClick={() => updateStatus('led-one', 1)}>Turn LED One On</button>
+        <button onClick={() => updateStatus('led-one', 0)}>Turn LED One Off</button>
+      </div>
+      <div>
+        <button onClick={() => updateStatus('led-two', 1)}>Turn LED Two On</button>
+        <button onClick={() => updateStatus('led-two', 0)}>Turn LED Two Off</button>
+      </div>
+      <div>
+        <button onClick={() => updateStatus('led-three', 1)}>Turn LED Three On</button>
+        <button onClick={() => updateStatus('led-three', 0)}>Turn LED Three Off</button>
+      </div>
+      <div>
+        <button onClick={() => updateStatus('led-four', 1)}>Turn LED Four On</button>
+        <button onClick={() => updateStatus('led-four', 0)}>Turn LED Four Off</button>
+      </div>
     </div>
   );
 }
