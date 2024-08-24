87a5d188b54e3c3013449a7ed17bbfb9b46a872b
celalcan <celalcanh.h.oglu@gmail.com>
2024-08-24 15:58:41 +0300

    Commit mesajınız

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import axios from 'axios';
 import React from 'react';
 
 function App() {
+  const fetchCurrentStatus = async () => {
+    try {
+      const response = await axios.get('https://api-control-on-off.vercel.app/api/status');
+      return response.data.status;
+    } catch (error) {
+      console.error('Error fetching current status:', error);
+      return {};
+    }
+  };
+
   const handleStatusChange = async (led, newValue) => {
     try {
-      const response = await axios.patch('https://api-control-on-off.vercel.app/api/status', { 
-        status: { 
-          [led]: newValue 
-        } 
-      });
+      const currentStatus = await fetchCurrentStatus();
+      const updatedStatus = { ...currentStatus, [led]: newValue };
+
+      const response = await axios.patch('https://api-control-on-off.vercel.app/api/status', { status: updatedStatus });
       console.log(`Status of ${led} updated:`, response.data);
     } catch (error) {
       console.error(`There was an error updating the status of ${led}!`, error);
